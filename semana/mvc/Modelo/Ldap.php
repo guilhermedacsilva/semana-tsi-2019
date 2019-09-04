2a5e41da5e3730566d79c8d28055d75fd6531802
Guilherme da Costa Silva <guilhermedacsilva@gmail.com>
2019-09-04 19:12:13 -0300

    Teste ldap

--- semana/mvc/Modelo/Ldap.php
+++ semana/mvc/Modelo/Ldap.php
@@ -6,13 +6,16 @@ class Ldap extends Modelo
 	public static function buscarNome($uid, $senha)
 	{
 		$conexao = \ldap_connect(LDAP_URI);
+		echo 1;
 		if ($conexao) {
+			echo 2;
 		    \ldap_set_option($conexao, LDAP_OPT_PROTOCOL_VERSION, 3);
     		\ldap_set_option($conexao, LDAP_OPT_REFERRALS, 0);
     	} else {
     		throw new Exception("ERRO AO CONECTAR AO DNS", 1);
     	}
 		$bind = false;
+		echo 3;
 		foreach (BASE_DNS_ARRAY as $baseDns) {
 			$baseDnString = "uid={$uid},{$baseDns}";
 			$bind = \ldap_bind($conexao, $baseDnString, $senha);
@@ -20,13 +23,16 @@ class Ldap extends Modelo
 				break;
 			}
 	    }
+		echo 4;
 	    if (!$bind) {
 	    	return null;
 	    }
+		echo 5;
 	    $leitor = \ldap_read($conexao, $baseDnString, LDAP_FILTRO, LDAP_ATRIBUTOS_ARRAY);
 	    if (!$leitor) {
 	    	throw new Exception("ERRO DO LDAP: ldap_read", 1);
 	    }
+		echo 6;
 	    $informacoes = \ldap_get_entries($conexao, $leitor);
 	    if (!$informacoes) {
 	    	throw new Exception("ERRO DO LDAP: ldap_get_entries", 1);
